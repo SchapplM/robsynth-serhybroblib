ed213741b9f7d752f0ca431664f510220b970d16
Moritz Schappler (IRTPC023) <schappler@irt.uni-hannover.de>
2017-12-20 19:25:20 +0100

    Gesamttest

---
+++ hybrid_mdl_test_all_models.m
@@ -0,0 +1,8 @@
+tb_path = fullfile(fileparts(which('hybrid_mdl_test_path_init.m')));
+
+KAS5m5_test_everything_fixbase
+KAS5m5u_test_everything_fixbase
+KAS5m6_test_everything_fixbase
+KAS5m6u_test_everything_fixbase
+KAS7m1_test_everything_fixbase
+KAS7m1u_test_everything_fixbase
