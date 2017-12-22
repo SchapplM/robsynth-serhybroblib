8f3163ac4f2fa4eec5688d7d2766df28c72f0691
Moritz Schappler (IRTPC023) <schappler@irt.uni-hannover.de>
2017-12-22 15:38:43 +0100

    Anpassung Gesamttest: Nutze nicht die Funktionen aus 3.Arm-Repo

--- hybrid_mdl_test_all_models.m
+++ hybrid_mdl_test_all_models.m
@@ -1,4 +1,9 @@
 tb_path = fullfile(fileparts(which('hybrid_mdl_test_path_init.m')));
+addpath(tb_path);
+hybrid_mdl_test_path_init
+
+KAS_path = fullfile(fileparts(which('KAS_path.m')));
+rmpath(genpath(KAS_path));
 
 KAS5m5_test_everything_fixbase
 KAS5m5u_test_everything_fixbase
@@ -6,3 +11,4 @@ KAS5m6_test_everything_fixbase
 KAS5m6u_test_everything_fixbase
 KAS7m1_test_everything_fixbase
 KAS7m1u_test_everything_fixbase
+
