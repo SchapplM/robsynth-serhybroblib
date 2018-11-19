f74163aeaa966f33436ff730111fc214d4c3447c
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2018-11-19 19:29:44 +0100

    [fourbar] Code-Generierung parallel

--- systems/fourbar1/codegen/generate_maple_code.sh
+++ systems/fourbar1/codegen/generate_maple_code.sh
@@ -23,5 +23,5 @@ cd $maplerepopath
 for df in $deflist; do
   echo "Starte Generierung für $df"
   cp robot_codegen_definitions/$df robot_codegen_definitions/robot_env
-  ./robot_codegen_start.sh --fixb_only --notest
+  ./robot_codegen_start.sh -p --fixb_only --notest
 done
