b37980fb41034b96f74b1b2570457a0842b9fe62
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2019-02-26 19:09:53 +0100

    [test] Weitere Systeme in Gesamttest aufgenommen

--- hybroblib_test_all_models.m
+++ hybroblib_test_all_models.m
@@ -1,18 +1,31 @@
 % Starte die Modultests für alle generierten Robotermodelle
 % 
 % Moritz Schappler, schappler@imes.uni-hannover.de, 2018-04
-% (C) Institut für mechatronische Systeme, Universität Hannover
+% (C) Institut für Mechatronische Systeme, Universität Hannover
 
 clc
 clear
 
-
 %% Viergelenkkette
 systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
 run(fullfile(systems_path, 'fourbar1', 'fourbar1_test.m'));
+
+%% Fünfgelenkkette
+systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
+run(fullfile(systems_path, 'fivebar1', 'fivebar1_test.m'));
+
 %% MPL800-Yaskawa
 systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
 run(fullfile(systems_path, 'palh1m1', 'palh1m1_test.m'));
+
+%% Palettierer-Ersatzmodell
+systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
+run(fullfile(systems_path, 'palh2m1', 'palh2m1_test_constr_dynamics_class.m'));
+
+%% 2D-Delta
+systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
+run(fullfile(systems_path, 'TSR', 'TSR_test.m'));
+
 return
 %% 3. Arm
 % TODO: Hier gibt es noch Pfad-Probleme
