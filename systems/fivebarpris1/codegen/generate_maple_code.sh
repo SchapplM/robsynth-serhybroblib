252f21eefe3a04bc005900d4672cc09abbd4c307
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-04-07 18:14:35 +0100

    [fivebarpris1] Code-Generierung korrigiert (bessere Standardwerte zum Testen, neues Testskript mit Beispielparametern)

--- systems/fivebarpris1/codegen/generate_maple_code.sh
+++ systems/fivebarpris1/codegen/generate_maple_code.sh
@@ -23,9 +23,9 @@ cd $maplerepopath
 for df in $deflist; do
   echo "Starte Generierung für $df"
   cp robot_codegen_definitions/$df robot_codegen_definitions/robot_env
-  ./robot_codegen_start.sh -p --fixb_only --minimal
+  ./robot_codegen_start.sh -p --fixb_only
 done
 
 
 cp robot_codegen_definitions/robot_env_fivebarpris1OL robot_codegen_definitions/robot_env
-./robot_codegen_start.sh --fixb_only --ic --minimal
+./robot_codegen_start.sh --fixb_only --ic
---
+++ systems/fivebarpris1/fivebarpris1_test.m
@@ -0,0 +1,44 @@
+% Teste fivebarpris1
+
+% Quelle:
+
+% Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2020-04
+% (C) Institut für Mechatronische Systeme, Leibniz Universität Hannover
+
+clear
+clc
+
+%% Definition der Roboterklassen
+RS_TE = hybroblib_create_robot_class('fivebarpris1', 'TE', 'fivebarpris1Bsp1');
+RS_DE1 = hybroblib_create_robot_class('fivebarpris1', 'DE1', 'fivebarpris1Bsp1');
+RS_DE2 = hybroblib_create_robot_class('fivebarpris1', 'DE2', 'fivebarpris1Bsp1');
+
+TSS = RS_TE.gen_testsettings();
+%% Vergleich der Implementierungen
+for i = 1:TSS.n
+  q=TSS.Q(i,:)';
+  T_DE1 = RS_DE1.fkine(q);
+  T_DE2 = RS_DE2.fkine(q);
+  T_TE = RS_TE.fkine(q);
+  test1 = T_DE1-T_DE2;
+  test2 = T_DE1-T_TE;
+ if any(abs([test1(:); test2(:)]) > 1e-10)
+   error('Methoden DE1, DE2 und TE stimmen nicht überein');
+ end
+end
+fprintf('Kinematik vom Typ KUKA KR-700PA für 1000 Kombinationen in unterschiedlichen Implementierungen getestet\n');
+
+%% Roboter plotten
+s_plot = struct( 'ks', [1:RS_DE1.NJ, RS_DE1.NJ+2], 'straight', 1);
+q = [0;NaN;120;0;40]*pi/180;
+q(2) = 1; % D-Parameter
+figure(2);clf;
+hold on;
+grid on;
+xlabel('x [m]');
+ylabel('y [m]');
+zlabel('z [m]');
+view(3);
+title([RS_DE1.descr, ' - Nullstellung']);
+RS_TE.plot( q, s_plot );
+
