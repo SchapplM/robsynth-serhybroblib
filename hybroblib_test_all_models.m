3b732c3675e45ad34f43c94f283bf54ca16669f3
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-04-11 16:20:01 +0100

    Code und Beispiele für 3. Arm ("KAS") entfernt. War sowieso nicht mehr kompatibel zur Robotik-Toolbox

--- hybroblib_test_all_models.m
+++ hybroblib_test_all_models.m
@@ -1,7 +1,7 @@
 % Starte die Modultests für alle generierten Robotermodelle
 % 
 % Moritz Schappler, schappler@imes.uni-hannover.de, 2018-04
-% (C) Institut für Mechatronische Systeme, Universität Hannover
+% (C) Institut für Mechatronische Systeme, Leibniz Universität Hannover
 
 clc
 clear
@@ -44,35 +44,3 @@ systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
 run(fullfile(systems_path, 'picker2Dm1', 'picker2Dm1_test.m'));
 systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
 run(fullfile(systems_path, 'picker2Dm2', 'picker2Dm2_test.m'));
-return
-%% 3. Arm
-% TODO: Hier gibt es noch Pfad-Probleme
-systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
-cd(fullfile(systems_path, 'KAS5m5', 'testfcn'));
-% Die Funktionen jedes Robotermodells werden für sich getestet
-KAS5m5_test_everything_fixbase
-KAS5m5u_test_everything_fixbase
-systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
-cd(fullfile(systems_path, 'KAS5m5'));
-KAS5m5_test_constr_dynamics
-
-systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
-cd(fullfile(systems_path, 'KAS5m6', 'testfcn'));
-KAS5m6_test_everything_fixbase
-KAS5m6u_test_everything_fixbase
-systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
-cd(fullfile(systems_path, 'KAS5m6'));
-KAS5m6_test_constr_dynamics
-
-systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
-cd(fullfile(systems_path, 'KAS5m7', 'testfcn'));
-KAS7m1_test_everything_fixbase
-KAS7m1u_test_everything_fixbase
-systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
-cd(fullfile(systems_path, 'KAS5m7'));
-KAS5m7_test_constr_dynamics
-
-systems_path = fullfile(fileparts(which('hybroblib_path_init.m')), 'systems');
-cd(fullfile(systems_path, 'KAS7m1'));
-KAS7m1_test_constr_dynamics
-
