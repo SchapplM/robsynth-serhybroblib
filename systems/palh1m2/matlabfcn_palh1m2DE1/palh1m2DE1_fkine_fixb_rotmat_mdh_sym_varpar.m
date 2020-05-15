d7e36d822d88f498e4402076d3f63e5ecb742094
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-05-15 14:44:31 +0100

    Zusätzliche Ausgabe für alle Kinematik-Funktionen: gestapelte Transformationsmatrizen

--- systems/palh1m2/matlabfcn_palh1m2DE1/palh1m2DE1_fkine_fixb_rotmat_mdh_sym_varpar.m
+++ systems/palh1m2/matlabfcn_palh1m2DE1/palh1m2DE1_fkine_fixb_rotmat_mdh_sym_varpar.m
@@ -24,7 +24,7 @@
 % Moritz Schappler, moritz.schappler@imes.uni-hannover.de
 % (C) Institut für Mechatronische Systeme, Universität Hannover
 
-function T_c_mdh = palh1m2DE1_fkine_fixb_rotmat_mdh_sym_varpar(qJ, pkin)
+function [T_c_mdh, Tc_stack] = palh1m2DE1_fkine_fixb_rotmat_mdh_sym_varpar(qJ, pkin)
 %% Coder Information
 %#codegen
 %$cgargs {zeros(4,1),zeros(22,1)}
@@ -152,3 +152,5 @@ else,                         T_c_mdh = sym('xx', [4,4,16+1]); end % symbolisch
 for i = 1:16+1
   T_c_mdh(:,:,i) = T_ges((i-1)*4+1 : 4*i, :);
 end
+Tc_stack = NaN(3*size(T_c_mdh,3),4);
+for i = 1:size(T_c_mdh,3), Tc_stack((i-1)*3+1:3*i,1:4) = T_c_mdh(1:3,1:4,i); end
